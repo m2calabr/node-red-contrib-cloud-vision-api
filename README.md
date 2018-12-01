586049f3923daa8c935bb69924aa414289557cdd
zuhito <zuhito@hotmail.co.jp>
2018-12-01 17:22:28 +0900

    Update spec

--- README.md
+++ README.md
@@ -17,43 +17,8 @@ Usage
 
 ### Methods
 
-- visionfilesasyncBatchAnnotate
-
-    Run asynchronous image detection and annotation for a list of generic
-files, such as PDF files, which may contain multiple pages and multiple
-images per page. Progress and results can be retrieved through the
-`google.longrunning.Operations` interface.
-`Operation.metadata` contains `OperationMetadata` (metadata).
-`Operation.response` contains `AsyncBatchAnnotateFilesResponse` (results).
-
-- visionimagesannotate
+- annotate
 
     Run image detection and annotation for a batch of images.
-
-- visionoperationsdelete
-
-    Deletes a long-running operation. This method indicates that the client is
-no longer interested in the operation result. It does not cancel the
-operation. If the server doesn't support this method, it returns
-`google.rpc.Code.UNIMPLEMENTED`.
-
-- visionlocationsoperationsget
-
-    Gets the latest state of a long-running operation.  Clients can use this
-method to poll the operation result at intervals as recommended by the API
-service.
-
-- visionoperationscancel
-
-    Starts asynchronous cancellation on a long-running operation.  The server
-makes a best effort to cancel the operation, but success is not
-guaranteed.  If the server doesn't support this method, it returns
-`google.rpc.Code.UNIMPLEMENTED`.  Clients can use
-Operations.GetOperation or
-other methods to check whether the cancellation succeeded or whether the
-operation completed despite cancellation. On successful cancellation,
-the operation is not deleted; instead, it becomes an operation with
-an Operation.error value with a google.rpc.Status.code of 1,
-corresponding to `Code.CANCELLED`.
 
 
--- test/node_spec.js
+++ test/node_spec.js
@@ -1,6 +1,6 @@
-var should = require("should");
-var helper = require("node-red-node-test-helper");
-var node = require("../node.js");
+var should = require('should');
+var helper = require('node-red-node-test-helper');
+var node = require('../node.js');
 
 helper.init(require.resolve('node-red'));
 
@@ -19,38 +19,29 @@ describe('cloud-vision-api node', function () {
     });
 
     it('should be loaded', function (done) {
-        var flow = [{ id: "n1", type: "cloud-vision-api", name: "cloud-vision-api" }];
+        var flow = [{ id: 'n1', type: 'cloud-vision-api', name: 'cloud-vision-api' }];
         helper.load(node, flow, function () {
-            var n1 = helper.getNode("n1");
+            var n1 = helper.getNode('n1');
             n1.should.have.property('name', 'cloud-vision-api');
             done();
         });
     });
 
-    it('should handle visionfilesasyncBatchAnnotate()', function (done) {
+    it('should handle annotate()', function (done) {
         var flow = [
-            { id: "n1", type: "cloud-vision-api", name: "cloud-vision-api", wires: [["n2"]],
-                method: "visionfilesasyncBatchAnnotate",
-                visionfilesasyncBatchAnnotate_body: "<node property>", // (1) define node properties
-                visionfilesasyncBatchAnnotate_key: "<node property>", // (1) define node properties
-                visionfilesasyncBatchAnnotate_accessToken: "<node property>", // (1) define node properties
-                visionfilesasyncBatchAnnotate_uploadProtocol: "<node property>", // (1) define node properties
-                visionfilesasyncBatchAnnotate_quotaUser: "<node property>", // (1) define node properties
-                visionfilesasyncBatchAnnotate_prettyPrint: "<node property>", // (1) define node properties
-                visionfilesasyncBatchAnnotate_uploadType: "<node property>", // (1) define node properties
-                visionfilesasyncBatchAnnotate_fields: "<node property>", // (1) define node properties
-                visionfilesasyncBatchAnnotate_callback: "<node property>", // (1) define node properties
-                visionfilesasyncBatchAnnotate_oauthToken: "<node property>", // (1) define node properties
-                visionfilesasyncBatchAnnotate_xgafv: "<node property>", // (1) define node properties
-                visionfilesasyncBatchAnnotate_alt: "<node property>", // (1) define node properties
-                service: "n3" },
-            { id: "n2", type: "cloud-vision-api-service" },
-            { id: "n3", type: "helper" }
+            { id: 'n1', type: 'cloud-vision-api', name: 'cloud-vision-api',
+                method: 'annotate',
+                annotate_body: '<node property>', // (1) define node properties
+                annotate_type: '<node property>', // (1) define node properties
+                annotate_key: '<node property>', // (1) define node properties
+                wires: [['n3']]
+            },
+            { id: 'n3', type: 'helper' }
         ];
         helper.load(node, flow, function () {
-            var n3 = helper.getNode("n3");
-            var n1 = helper.getNode("n1");
-            n3.on("input", function (msg) {
+            var n3 = helper.getNode('n3');
+            var n1 = helper.getNode('n1');
+            n3.on('input', function (msg) {
                 try {
                     msg.should.have.property('payload', '<output message>'); // (3) define output message
                     done();
@@ -58,145 +49,7 @@ describe('cloud-vision-api node', function () {
                     done(e);
                 }
             });
-            n1.receive({ payload: "<input message>" }); // (2) define input message
-        });
-    });
-    it('should handle visionimagesannotate()', function (done) {
-        var flow = [
-            { id: "n1", type: "cloud-vision-api", name: "cloud-vision-api", wires: [["n2"]],
-                method: "visionimagesannotate",
-                visionimagesannotate_body: "<node property>", // (1) define node properties
-                visionimagesannotate_key: "<node property>", // (1) define node properties
-                visionimagesannotate_accessToken: "<node property>", // (1) define node properties
-                visionimagesannotate_uploadProtocol: "<node property>", // (1) define node properties
-                visionimagesannotate_quotaUser: "<node property>", // (1) define node properties
-                visionimagesannotate_prettyPrint: "<node property>", // (1) define node properties
-                visionimagesannotate_uploadType: "<node property>", // (1) define node properties
-                visionimagesannotate_fields: "<node property>", // (1) define node properties
-                visionimagesannotate_callback: "<node property>", // (1) define node properties
-                visionimagesannotate_oauthToken: "<node property>", // (1) define node properties
-                visionimagesannotate_xgafv: "<node property>", // (1) define node properties
-                visionimagesannotate_alt: "<node property>", // (1) define node properties
-                service: "n3" },
-            { id: "n2", type: "cloud-vision-api-service" },
-            { id: "n3", type: "helper" }
-        ];
-        helper.load(node, flow, function () {
-            var n3 = helper.getNode("n3");
-            var n1 = helper.getNode("n1");
-            n3.on("input", function (msg) {
-                try {
-                    msg.should.have.property('payload', '<output message>'); // (3) define output message
-                    done();
-                } catch (e) {
-                    done(e);
-                }
-            });
-            n1.receive({ payload: "<input message>" }); // (2) define input message
-        });
-    });
-    it('should handle visionoperationsdelete()', function (done) {
-        var flow = [
-            { id: "n1", type: "cloud-vision-api", name: "cloud-vision-api", wires: [["n2"]],
-                method: "visionoperationsdelete",
-                visionoperationsdelete_name: "<node property>", // (1) define node properties
-                visionoperationsdelete_key: "<node property>", // (1) define node properties
-                visionoperationsdelete_accessToken: "<node property>", // (1) define node properties
-                visionoperationsdelete_uploadProtocol: "<node property>", // (1) define node properties
-                visionoperationsdelete_quotaUser: "<node property>", // (1) define node properties
-                visionoperationsdelete_prettyPrint: "<node property>", // (1) define node properties
-                visionoperationsdelete_uploadType: "<node property>", // (1) define node properties
-                visionoperationsdelete_fields: "<node property>", // (1) define node properties
-                visionoperationsdelete_callback: "<node property>", // (1) define node properties
-                visionoperationsdelete_oauthToken: "<node property>", // (1) define node properties
-                visionoperationsdelete_xgafv: "<node property>", // (1) define node properties
-                visionoperationsdelete_alt: "<node property>", // (1) define node properties
-                service: "n3" },
-            { id: "n2", type: "cloud-vision-api-service" },
-            { id: "n3", type: "helper" }
-        ];
-        helper.load(node, flow, function () {
-            var n3 = helper.getNode("n3");
-            var n1 = helper.getNode("n1");
-            n3.on("input", function (msg) {
-                try {
-                    msg.should.have.property('payload', '<output message>'); // (3) define output message
-                    done();
-                } catch (e) {
-                    done(e);
-                }
-            });
-            n1.receive({ payload: "<input message>" }); // (2) define input message
-        });
-    });
-    it('should handle visionlocationsoperationsget()', function (done) {
-        var flow = [
-            { id: "n1", type: "cloud-vision-api", name: "cloud-vision-api", wires: [["n2"]],
-                method: "visionlocationsoperationsget",
-                visionlocationsoperationsget_name: "<node property>", // (1) define node properties
-                visionlocationsoperationsget_key: "<node property>", // (1) define node properties
-                visionlocationsoperationsget_accessToken: "<node property>", // (1) define node properties
-                visionlocationsoperationsget_uploadProtocol: "<node property>", // (1) define node properties
-                visionlocationsoperationsget_quotaUser: "<node property>", // (1) define node properties
-                visionlocationsoperationsget_prettyPrint: "<node property>", // (1) define node properties
-                visionlocationsoperationsget_uploadType: "<node property>", // (1) define node properties
-                visionlocationsoperationsget_fields: "<node property>", // (1) define node properties
-                visionlocationsoperationsget_callback: "<node property>", // (1) define node properties
-                visionlocationsoperationsget_oauthToken: "<node property>", // (1) define node properties
-                visionlocationsoperationsget_xgafv: "<node property>", // (1) define node properties
-                visionlocationsoperationsget_alt: "<node property>", // (1) define node properties
-                service: "n3" },
-            { id: "n2", type: "cloud-vision-api-service" },
-            { id: "n3", type: "helper" }
-        ];
-        helper.load(node, flow, function () {
-            var n3 = helper.getNode("n3");
-            var n1 = helper.getNode("n1");
-            n3.on("input", function (msg) {
-                try {
-                    msg.should.have.property('payload', '<output message>'); // (3) define output message
-                    done();
-                } catch (e) {
-                    done(e);
-                }
-            });
-            n1.receive({ payload: "<input message>" }); // (2) define input message
-        });
-    });
-    it('should handle visionoperationscancel()', function (done) {
-        var flow = [
-            { id: "n1", type: "cloud-vision-api", name: "cloud-vision-api", wires: [["n2"]],
-                method: "visionoperationscancel",
-                visionoperationscancel_name: "<node property>", // (1) define node properties
-                visionoperationscancel_body: "<node property>", // (1) define node properties
-                visionoperationscancel_key: "<node property>", // (1) define node properties
-                visionoperationscancel_accessToken: "<node property>", // (1) define node properties
-                visionoperationscancel_uploadProtocol: "<node property>", // (1) define node properties
-                visionoperationscancel_quotaUser: "<node property>", // (1) define node properties
-                visionoperationscancel_prettyPrint: "<node property>", // (1) define node properties
-                visionoperationscancel_uploadType: "<node property>", // (1) define node properties
-                visionoperationscancel_fields: "<node property>", // (1) define node properties
-                visionoperationscancel_callback: "<node property>", // (1) define node properties
-                visionoperationscancel_oauthToken: "<node property>", // (1) define node properties
-                visionoperationscancel_xgafv: "<node property>", // (1) define node properties
-                visionoperationscancel_alt: "<node property>", // (1) define node properties
-                service: "n3" },
-            { id: "n2", type: "cloud-vision-api-service" },
-            { id: "n3", type: "helper" }
-        ];
-        helper.load(node, flow, function () {
-            var n3 = helper.getNode("n3");
-            var n1 = helper.getNode("n1");
-            n3.on("input", function (msg) {
-                try {
-                    msg.should.have.property('payload', '<output message>'); // (3) define output message
-                    done();
-                } catch (e) {
-                    done(e);
-                }
-            });
-            n1.receive({ payload: "<input message>" }); // (2) define input message
+            n1.receive({ payload: '<input message>' }); // (2) define input message
         });
     });
 });
-
